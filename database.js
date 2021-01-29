7619eb89acd87ac84a3f906d9c9b1e94682d059d
shkarsardar <shkarhsardar@gmail.com>
2021-01-29 12:27:57 +0300

    add process env

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const mysql=require('mysql');
 module.exports=mysql.createConnection({
-    host:'localhost',
-    user:'mrprogrammez',
-    password:'toor',
-    database:'mydairy',
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
     dateStrings:true
 });
