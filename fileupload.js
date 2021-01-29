d77bf038de2d7990a7fa566523dcbb6733266fca
shkarsardar <shkarhsardar@gmail.com>
2021-01-29 13:18:28 +0300

    fix path in database.js

--- fileupload.js
+++ fileupload.js
@@ -1,10 +1,12 @@
 const multer=require('multer');
 const path=require('path');
 const megabyte=1*1000*1000;
+const root=path.resolve(__dirname);
+console.log(root);
 
 const storage=multer.diskStorage({
     destination:function(req,file,cb){
-        cb(null,'public/profiles/');
+        cb(null,root+'/public/profiles/');
     },
     filename:function(req,file,cb){
         cb(null,Date.now()+path.extname(file.originalname));
